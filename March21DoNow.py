733e8eeae5e2ab662672f23f4e1352bc8220f975
mommina <mommina.khalid@ms223.org>
2016-03-21 08:23:33 -0400

    Delete March21DoNow.py

--- March21DoNow.py
+++
@@ -1,8 +0,0 @@
-import random
-random.randint(0, 3) #randint 
-random.randint(0, 3) #the values 0 and 3 serve as the range
-print(random.randint(0, 3))
-print(random.randint(0, 3))
-print(random.randint(0, 3))
-#Everytime I let it run, the result is different numbers from 0-3
-
