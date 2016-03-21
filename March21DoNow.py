bc24fc11cd98c92a87766e22c6685ba2af594119
angie-ortiz <angie.ortiz@ms223.org>
2016-03-21 08:22:26 -0400

    Update March21DoNow.py

--- March21DoNow.py
+++ March21DoNow.py
@@ -4,3 +4,5 @@ random.randint(0, 3) #the values 0 and 3 serve as the range
 print(random.randint(0, 3))
 print(random.randint(0, 3))
 print(random.randint(0, 3))
+#Everytime I let it run, the result is different numbers from 0-3
+
