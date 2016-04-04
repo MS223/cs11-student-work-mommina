1183e07b27b76cea13f63cad4a5d858f04c6bb6b
mommina <mommina.khalid@ms223.org>
2016-04-04 08:42:31 -0400

    Update Guessing game.py

--- Guessing game.py
+++ Guessing game.py
@@ -1,4 +1,5 @@
-name = answer
-input "What is your name ?"
-Print " "
-# starting the greeting
+n = raw_input ("What is your name ?")
+q = input ("What is the highest value ?")
+g = input ("What number do you think it is ?")
+import random 
+# I need to add random, and than have feedback given to the player. 
