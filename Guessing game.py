b0e0d544b487c517aba667ff8ddd4bd30412aa53
mommina <mommina.khalid@ms223.org>
2016-04-05 08:54:11 -0400

    Update Guessing game.py

--- Guessing game.py
+++ Guessing game.py
@@ -1,5 +1,13 @@
+import random
 n = raw_input ("What is your name ?")
 q = input ("What is the highest value ?")
 g = input ("What number do you think it is ?")
-import random 
-# I need to add random, and than have feedback given to the player. 
+r = random.randint (1,q)
+while g != r:
+    if g < r:
+        print ("Too low!")
+        g = input ("What number do you think it is ?")
+    elif g > r:
+        print ("Too high!")
+        g = input ("What number do you think it is ?")
+print "You got it!"
