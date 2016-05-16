6d5e85adace8674132b416e1004658bccde08996
mommina <mommina.khalid@ms223.org>
2016-05-16 08:43:20 -0400

    Create Dictionaries Lab.py

---
+++ Dictionaries Lab.py
@@ -0,0 +1,18 @@
+my_profile = {
+'name': 'Mommina Khalid',
+'school': 'MS/HS223',
+'Birthday': 'Feb 26, 1999',
+'email': 'mommina.khalid@ms223.org',
+'from': 'Pakistan',
+'siblings': '0',
+'age': '17',
+'hobbies': 'dance',
+'favorite musician' : 'Arjit Singh',
+'favorite color' : 'Green',
+'zodiac sign': 'Pisces',
+}
+print my_profile['age']
+print my_profile.get('birthday')
+print 'name' in my_profile
+print 'from' in my_profile
+print my_profile ['siblings']
