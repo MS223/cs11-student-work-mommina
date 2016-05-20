7215e05362b6e75fca4f2c958c3780519aa89a14
mommina <mommina.khalid@ms223.org>
2016-05-20 08:44:05 -0400

    Create ToDo.py

---
+++ ToDo.py
@@ -0,0 +1,19 @@
+question = raw_input("What would you like to do?")
+day = raw_input ("What day ?")
+days_of_week = {
+"monday":[],
+"tuesday":[],
+"wednesday":[],
+"thursday":[],
+"friday":[],
+"saturday":[],
+"sunday":[]
+}
+def add():
+    question.append
+    days_of_week[day] = question
+print days_of_week
+
+# trying to get the computer to store the value which is the answer given by
+# someone so that when I print Print days of week it shows that I have something 
+# to do on whatever day that is chosen. 
