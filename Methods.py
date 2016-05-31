38028ffffdba69e2677eded0f110eb32f8c4cf36
mommina <mommina.khalid@ms223.org>
2016-05-31 09:24:15 -0400

    Update Methods.py

--- Methods.py
+++ Methods.py
@@ -5,12 +5,17 @@ class Time(object):
      self.second = second
 
  def __str__(self):
-     return str(self.hour) + ":" + str(self.minute)+ str(self.second)
+     return str(self.hour) + ":" + str(self.minute)+ ":" + str(self.second)
  def __add__(self, other):
-     
+     return Time( self.hour + other.hour,self.minute + other.hour, self.second + other.second)
+
+
 
 time1 = Time(5, 32, 0)
 time2 = Time(23, 11, 11)
+time3 = Time (9,8,7)
 
 print time1
 print time2
+
+print time1 +time2 +time3
