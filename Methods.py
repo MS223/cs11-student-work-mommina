9c9c1d7fe7d283bcfbf823fe3477b3d462c8eeb7
mommina <mommina.khalid@ms223.org>
2016-05-31 09:15:07 -0400

    Create Methods.py

---
+++ Methods.py
@@ -0,0 +1,16 @@
+class Time(object):
+ def __init__(self, hour, minute, second):
+     self.hour = hour
+     self.minute = minute
+     self.second = second
+
+ def __str__(self):
+     return str(self.hour) + ":" + str(self.minute)+ str(self.second)
+ def __add__(self, other):
+     
+
+time1 = Time(5, 32, 0)
+time2 = Time(23, 11, 11)
+
+print time1
+print time2
